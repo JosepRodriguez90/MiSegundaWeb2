c9894f1f42652c49941c02b722bff7f2674060c9
JosepRodriguez90 <jsprodriguez90@gmail.com>
2020-11-06 14:49:44 +0100

    tot

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { EventosComponent } from './components/eventos/eventos.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', pathMatch: 'full', redirectTo: 'home' },
+{ path: 'inicio', component: InicioComponent },
+{ path: 'eventos', component: EventosComponent },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { EventosComponent } from './components/eventos/eventos.component';
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    InicioComponent,
+    EventosComponent,
+    MainMenuComponent,
   ],
   imports: [
     BrowserModule,
