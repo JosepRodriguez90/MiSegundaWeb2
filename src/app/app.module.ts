cd01a282d3c50de9db90a9dcbc88ff8cdec6608f
JosepRodriguez90 <jsprodriguez90@gmail.com>
2020-11-11 19:32:58 +0100

    alta i borrar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { InicioComponent } from './components/inicio/inicio.component';
-import { EventosComponent } from './components/eventos/eventos.component';
+import { EventoComponent } from './components/eventos/eventos.component';
 import { MainMenuComponent } from './components/main-menu/main-menu.component';
-import { AltaEventoComponent } from './components/alta-evento/alta-evento.component';
-import { ModificarEventoComponent } from './components/modificar-evento/modificar-evento.component';
-import { EliminarEventoComponent } from './components/eliminar-evento/eliminar-evento.component';
 
 
 @NgModule({
@@ -17,12 +14,8 @@ import { EliminarEventoComponent } from './components/eliminar-evento/eliminar-e
     AppComponent,
     HeaderComponent,
     InicioComponent,
-    EventosComponent,
+    EventoComponent,
     MainMenuComponent,
-    AltaEventoComponent,
-    ModificarEventoComponent,
-    EliminarEventoComponent,
-    
   ],
   imports: [
     BrowserModule,
--- src/app/components/eventos/eventos.component.ts
+++ src/app/components/eventos/eventos.component.ts
@@ -5,12 +5,53 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './eventos.component.html',
   styleUrls: ['./eventos.component.css']
 })
-export class EventosComponent implements OnInit {
+export class EventoComponent implements OnInit {
+
+  AruedaEmail ='@';
+  mostrarInfo="";
+  users: string[] = [];
+
+  addUser(vName, vEmail, vLocalidad, vDescripcion) {
+    this.users.push(vName.value);
+    this.users.push(vEmail.value);
+    this.users.push(vLocalidad.value);
+    this.users.push(vDescripcion.value);
+
+    return false;
+  }
 
   constructor() { }
 
-  ngOnInit(): void {
+  Enviar(nombre, email, descripcion){
+
+    if(nombre.value ==""){
+      alert("El nombre esta vacio");
+    }
+
+    if(email.match(this.AruedaEmail)){
+      this.mostrarInfo="Correcto";
+    }else{
+      this.mostrarInfo="Incorrecto";
+    }
+
+    if(descripcion.value.length<2){
+      this.mostrarInfo="textocorto";
+    }
+
+  }
+ 
+
+  deleteUser(user) {
+
+    this.users
+    for(let i=0; i < this.users.length; i++) {
+      if(user == this.users[i]){
+        this.users.splice(i, 1);
+      }
+    }
   }
 
+  ngOnInit(): void {
+  }
 
 }
